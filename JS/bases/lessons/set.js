26fb4924972f5cc526d4a53558048192f1e3fd6f
Konstantin <maschkovc@yandex.ru>
2023-04-29 15:40:52 +0300

    test

--- JS/bases/lessons/set.js
+++ JS/bases/lessons/set.js
@@ -27,3 +27,26 @@ console.log(unique(arr));
 
 // console.log(set.values());
 
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('foo');
+    }, 300);
+});
+  
+promise.then((value) => {
+    console.log(value);
+});
+
+const promisify = (item, delay) =>
+    new Promise(resolve => setTimeout(() => resolve(item), delay));
+ 
+const a = () => promisify('a', 100);
+const b = () => promisify('b', 5000);
+const c = () => promisify('c', 3000);
+ 
+function one() {
+    const promises = [a(), b(), c()];
+    Promise.all(promises).then(values => console.log(values))
+}
+ 
+one();
