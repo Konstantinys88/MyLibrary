dcf5ece28751b804c76ab63baa175f782af592b5
Konstantin <maschkovc@yandex.ru>
2023-05-02 11:14:52 +0300

    get

---
+++ JS/bases/lessons/GeterSeter.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const person = {
+    name: "alex",
+    age: 25,
+
+    get userAge() {
+        return this.age;
+    },
+    set userAge(num) {
+        this.age = num;
+    }
+};
+
+console.log(person.userAge = 30);
+console.log(person.userAge);
+console.log(person.age);
