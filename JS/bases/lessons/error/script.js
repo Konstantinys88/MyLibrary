d89cb4812a8c7c41c3fca08be38388f1b3e4c1f5
Konstantin <maschkovc@yandex.ru>
2023-05-04 10:30:33 +0300

    Ошибки

---
+++ JS/bases/lessons/error/script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const data = [
+    {
+        id: 'box',
+        tag: 'div'
+    },
+    {
+        id: 'fff',
+        tag: 'nav'
+    },
+    {
+        id: 'circle',
+        tag: 'span'
+    }
+];
+
+
+try{
+    data.forEach((blockObj, i) => {
+        const block = document.createElement(blockObj.tag);
+    
+        if(!blockObj.id) throw new SyntaxError(`В данных плд номером ${i + 1} нет id`);
+    
+        block.setAttribute('id', blockObj.id);
+        document.body.append(block);
+    });
+} catch(e){
+    if (e.name === "SyntaxError") {
+        console.log(e.message);
+    } else throw e;
+}
+
+// const err = new SyntaxError('sfsdfdsf');
+// console.log(err.name, err.message, err.stack);
