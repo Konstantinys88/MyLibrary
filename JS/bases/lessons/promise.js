a5d0e99addb5a710449d8d65f659151952c5b6fc
Konstantin <maschkovc@yandex.ru>
2023-05-25 15:09:29 +0300

    promise

--- JS/bases/lessons/promise.js
+++ JS/bases/lessons/promise.js
@@ -2,43 +2,43 @@
 
 // console.log('Запрос данных...');
 
-// const req = new Promise(function(resolve, reject){
-//     setTimeout(() => {
-//         console.log('Подготовка данных...');
+const req = new Promise(function(resolve, reject){
+    setTimeout(() => {
+        console.log('Подготовка данных...');
     
-//         const product = {
-//             name: 'TV',
-//             price: 200,
-//         };
-//         resolve(product);
-//     }, 2000);
+        const product = {
+            name: 'TV',
+            price: 200,
+        };
+        resolve(product);
+    }, 2000);
 
-// });
+});
 
-// req.then((product) => {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             product.status = 'order';
-//             resolve(product);
-//             // reject();
-//         }, 2000);
-//     });
-// }).then(data => {
-//     data.modyfi = true;
-//     return data;
-// }).then(data => {
-//     console.log(data);
-// }).catch(() => {
-//     console.error("Произошла ошибка")
-// }).finally(() => {
-//     console.log('finali')
-// })
+req.then((product) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            product.status = 'order';
+            resolve(product);
+            reject();
+        }, 2000);
+    });
+}).then(data => {
+    data.modyfi = true;
+    return data;
+}).then(data => {
+    console.log(data);
+}).catch(() => {
+    console.error("Произошла ошибка")
+}).finally(() => {
+    console.log('finali')
+})
 
-const test = time => {
-    return new Promise(resolve => {
-        setTimeout(() => resolve(), time);
-    })
-}
+// const test = time => {
+//     return new Promise(resolve => {
+//         setTimeout(() => resolve(), time);
+//     })
+// }
 
 // test(1000).then(() => console.log('1000'))
 // test(2000).then(() => console.log('2000'))
